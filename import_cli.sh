7b50f8fc4611496194da39a8530663fa510adeea
kosuke.amano <kosuke.amano@nutrien.com>
2022-06-06 16:41:38 -0600

    add tf and import scripts

---
+++ import_cli.sh
@@ -0,0 +1,4 @@
+aws cloudformation create-change-set --stack-name cloudformation-man-sandbox --change-set-name ImportChangeSet --change-set-type IMPORT --resources-to-import file://sandbox_resources.txt --template-url https://test-template-koamano.s3.us-east-2.amazonaws.com/deployment.yaml --parameters file://sandbox_parameters.json --capabilities CAPABILITY_IAM
+
+# aws cloudformation describe-change-set --change-set-name ImportChangeSet --stack-name cloudformation-man-sandbox
+# aws cloudformation execute-change-set --change-set-name ImportChangeSet --stack-name cloudformation-man-sandbox
